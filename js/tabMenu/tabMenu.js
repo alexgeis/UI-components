5746795b5dda382a2a4097f9ecfcf509a8249f43
Alex Geis <siegxela@gmail.com>
2022-07-28 09:43:05 -0600

    add finalized tabMenu solution

--- js/tabMenu/tabMenu.js
+++ js/tabMenu/tabMenu.js
@@ -1,5 +1,14 @@
-function activeTab(tabName) {
-	console.log(tabName);
+//add event listener to all tab links
+const tablinks = document.getElementsByClassName("tab-link");
+for (let i = 0; i < tablinks.length; i++) {
+	tablinks[i].addEventListener("click", activeTab);
+}
+
+function activeTab(event) {
+	// Create the tab content ID by altering the incoming tab link ID
+	// input: "tab-link-1"  =>  output: "tab1"
+	const id = `tab${event.target.id.split("-")[2]}`;
+
 	// Get all elements with class="tabcontent" and hide them
 	const tabcontent = document.getElementsByClassName("tabcontent");
 	for (let i = 0; i < tabcontent.length; i++) {
@@ -11,20 +20,9 @@ function activeTab(tabName) {
 	for (let i = 0; i < tablinks.length; i++) {
 		tablinks[i].classList.remove("active");
 	}
-	console.log({ tablinks });
+
 	// Show the current tab, and add an "active" class to the button that opened the tab
-	const activeTab = document.getElementById(tabName);
-	console.log(activeTab);
+	const activeTab = document.getElementById(id);
 	activeTab.style.display = "block";
 	activeTab.classList.add("active");
 }
-
-const tabLink1 = document.getElementById("tab-link-1");
-const tabLink2 = document.getElementById("tab-link-2");
-const tabLink3 = document.getElementById("tab-link-3");
-const tabLink4 = document.getElementById("tab-link-4");
-
-tabLink1.addEventListener("click", activeTab("tab1"));
-tabLink2.addEventListener("click", activeTab("tab2"));
-// tabLink3.addEventListener("click", activeTab("tab3"));
-// tabLink4.addEventListener("click", activeTab("tab4"));
